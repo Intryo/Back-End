d95826849cfc6239db258ae428978118c618958e
souma9830 <prabirkumarshee8@gmail.com>
2026-01-03 18:03:00 +0530

    Authentication For new Register Done

---
+++ src/Controllers/AuthController.js
@@ -0,0 +1,31 @@
+import userModel from "../Models/UserModel";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register=async(req,res)=>{
+    const {name,email,password}=req.body;
+    if(!name || !email || !password){
+        return res.json({success:false,message:"Missing Details. Required Field Must be submitted"});
+    }
+   try {
+        const existing_user=await userModel.findOne({email});
+        if(existing_user){
+            return res.json({success:false,message:"User already Exist, Please Login"});
+        }
+        const hashpassword=await bcrypt.hash(password,10);
+        const user=new userModel({name,email,password:hashpassword});
+        await user.save();
+        const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn: '7d'});
+        res.cookies('token',token,{
+            httpOnly:true,
+            secure:process.env.NODE_ENV==='production',
+            sameSite:none,
+            maxAge:7*24*60*60*100
+        })
+
+   } catch (error) {
+        res.json({success:false,message:error.message})
+    
+   }
+
+}
