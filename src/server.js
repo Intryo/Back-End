a9934962b473101e21af1f69a7f2861d4c40ad81
souma9830 <prabirkumarshee8@gmail.com>
2026-01-03 15:15:02 +0530

    Serversite Mongodb Database Connect

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import connectdb from "./config/mongodb.js";
+connectdb();
 const app=express();
 const port= process.env.PORT ||  4000;
 app.use(express.json());
