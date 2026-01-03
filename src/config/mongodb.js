b49399b964646344a12cc33883a148ac1addc11c
souma9830 <prabirkumarshee8@gmail.com>
2026-01-03 15:16:08 +0530

    Mongodb Server Created

---
+++ src/config/mongodb.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+const connectdb=async()=>{
+    try {
+        mongoose.connection.on("connected",()=>{
+            console.log("Database Connected");
+        })
+         await mongoose.connect(`${process.env.MONGODB_URI}/intryo_userAuth`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+export default connectdb;
