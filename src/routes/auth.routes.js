0da92b05b5b2e63e2c18366592079ec3a1fb94d3
souma9830 <prabirkumarshee8@gmail.com>
2026-01-04 20:33:28 +0530

    new Route Added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 import express from "express"
-import {register,login,logout} from "../Controllers/AuthController.js";
+import {register,login,logout, sendverifyOtp, verifyMail} from "../Controllers/AuthController.js";
+import userAuth from "../../middleware/userAuth.js";
 const authRoute=express.Router();
 authRoute.post("/register",register);
 authRoute.post("/login",login);
 authRoute.post("/logout",logout);
-authRoute.post("/send-verify-otp",)
+authRoute.post("/send-verify-otp",userAuth,sendverifyOtp);
+authRoute.post("/verify-account",userAuth,verifyMail);
 export default authRoute;
