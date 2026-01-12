2ea5ec718437f9e72d69ed3c0277e1569ca0bc9d
souma9830 <prabirkumarshee8@gmail.com>
2026-01-13 00:07:27 +0530

    edit profile route add

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,10 @@
 import express from "express";
 import userauth from "../middleware/userAuth.js";
 import { getUserById, getUserData } from "../Controllers/userController.js";
+import { editprofile } from "../Controllers/AuthController.js";
+import upload from "../middleware/multer.js";
 const userRouter=express.Router();
 userRouter.get("/data", userauth, getUserData);
 userRouter.get("/:userId",getUserById)
+userRouter.patch("/profile/edit",userauth,upload.single('profilepicture'),editprofile)
 export default userRouter;
