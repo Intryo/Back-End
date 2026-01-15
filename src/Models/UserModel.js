f08c8766c57a6c60f9621da498a1f5c19c5bd67d
souma9830 <prabirkumarshee8@gmail.com>
2026-01-15 23:14:48 +0530

    Chnages

--- src/Models/UserModel.js
+++ src/Models/UserModel.js
@@ -1,25 +1,25 @@
 import mongoose from "mongoose";
-const UserSchema=new mongoose.Schema({
-    name:{type:String, required:true},
-    email:{type:String, required:true,unique:true},
-    password:{type:String,required:true},
-    verifyOtp:{type:String,default:''},
-    verifyOtpexpairy:{type:Number,default:0},
-    isVerified:{type:Boolean,default:false},
-    resendOtp:{type:String,default:''},
-    resendOtpExpairy:{type:Number,default:0},
-    passwordchangeotpVerify:{type:Boolean,default:false},
-    username: {type: String,unique: true,sparse: true,},
-    bio: {type: String,maxlength: 500,default: ""},
-    profilepicture: {type: String,default: "" },
-    friendsCount: {type: Number,default: 0},
-    posts:[{type:mongoose.Schema.Types.ObjectId, ref:'post'}],
-    bookmark:[{type:mongoose.Schema.Types.ObjectId,ref:'post'}],
-    sociallinks:[{type:[String],default:[]}],
-    fieldofintereset:[{type:[String],default:[]}],
-    fullfillmentpoint:{type:Number,default:50}
-  },
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  verifyOtp: { type: String, default: '' },
+  verifyOtpexpairy: { type: Number, default: 0 },
+  isVerified: { type: Boolean, default: false },
+  resendOtp: { type: String, default: '' },
+  resendOtpExpairy: { type: Number, default: 0 },
+  passwordchangeotpVerify: { type: Boolean, default: false },
+  username: { type: String, unique: true, sparse: true, },
+  bio: { type: String, maxlength: 500, default: "" },
+  profilepicture: { type: String, default: "" },
+  friendsCount: { type: Number, default: 0 },
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'post' }],
+  bookmark: [{ type: mongoose.Schema.Types.ObjectId, ref: 'post' }],
+  sociallinks: { type: [String], default: [] },
+  fieldofintereset: { type: [String], default: [] },
+  fullfillmentpoint: { type: Number, default: 50 }
+},
   { timestamps: true }
 )
-const userModel=mongoose.models.user|| mongoose.model('user',UserSchema);
+const userModel = mongoose.models.user || mongoose.model('user', UserSchema);
 export default userModel;
